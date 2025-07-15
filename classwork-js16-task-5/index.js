dfc09983024975be0e94a6bacbda22a307341e87
perai0980@gmail.com <perai0980@gmail.com>
2025-07-15 21:20:14 +0300

    im commiting

--- classwork-js16-task-5/index.js
+++ classwork-js16-task-5/index.js
@@ -1,15 +1,17 @@
 const form = document.querySelector("form");
+const select = document.querySelector("#dropdown");
+
+fetch("https://api.chucknorris.io/jokes/categories")
+  .then((responce) => responce.json())
+  .then((data) => {
+    getCategorie(data);
+  })
+  .catch((error) => console.error("Error fetching data:", error));
 
 function getCategorie() {
-  fetch("https://api.chucknorris.io/jokes/categories")
-    .then((responce) => responce.json())
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((error) => console.error("Error fetching data:", error));
-  const select = document.querySelector("#dropdown");
-
   const newOption = document.createElement("option");
+  newOption.textContent = "new";
   select.append(newOption);
+  console.log(select);
 }
 getCategorie();
